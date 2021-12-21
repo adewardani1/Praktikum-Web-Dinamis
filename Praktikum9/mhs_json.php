095dd716100b0975edf6f7777abba19a452d7299
adewardani1 <adewardani9e@gmail.com>
2021-12-21 19:06:28 +0800

    Praktikum 9

--- Praktikum9/mhs_json.php
+++ Praktikum9/mhs_json.php
@@ -1,11 +1,18 @@
 <?php
+//panggil koneksi database
 include "koneksi.php";
+
+//query tabel Mahasiswa
 $sql = "select * from mahasiswa order by nim";
 $tampil = mysqli_query($con, $sql);
+
+//percabangan
 if (mysqli_num_rows($tampil) > 0) {
     $no = 1;
+    //buat data mahasiswa menjadi array
     $response = array();
     $response["data"] = array();
+    //looping data dari query mahasiswa
     while ($r = mysqli_fetch_array($tampil)) {
         $h['nim'] = $r['nim'];
         $h['nama'] = $r['nama'];
@@ -14,8 +21,10 @@ if (mysqli_num_rows($tampil) > 0) {
         $h['tgllhr'] = $r['tgllhr'];
         array_push($response["data"], $h);
     }
+    //mengubah data array menjadi data json
     echo json_encode($response);
 } else {
+    //jika data tidak ada maka akan menampilkan "tidak ada data"
     $response["message"] = "tidak ada data";
     echo json_encode($response);
 }
--- Praktikum9/mhs_xml.php
+++ Praktikum9/mhs_xml.php
@@ -1,11 +1,20 @@
 <?php
 include "koneksi.php";
+
+//merupakan header file XML
 header('Content-Type: text/xml');
+
+//query tabel mahasiswa
 $query = "SELECT * FROM mahasiswa";
 $hasil = mysqli_query($con, $query);
 $jumField = mysqli_num_fields($hasil);
+
 echo "<?xml version='1.0'?>";
+
+//parent node xml
 echo "<data>";
+echo $jumField;
+//looping  data dari query mahasiswa
 while ($data = mysqli_fetch_array($hasil)) {
     echo "<mahasiswa>";
     echo "<nim>", $data['nim'], "</nim>";
@@ -15,4 +24,6 @@ while ($data = mysqli_fetch_array($hasil)) {
     echo "<tgllhr>", $data['tgllhr'], "</tgllhr>";
     echo "</mahasiswa>";
 }
+
+// merupakan akhir file atau tag penutup node 
 echo "</data>";
